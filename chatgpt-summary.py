8ad5c4ba562d814f3c25c26ef22e57207f3b2dbf
ahmethaksever <107223224+ahmethaksever@users.noreply.github.com>
2023-08-06 15:10:15 +0300

    Update chatgpt-summary.py

--- chatgpt-summary.py
+++ chatgpt-summary.py
@@ -3,7 +3,7 @@ import openai
 openai.api_key = "OPENAI_TOKEN"
 
 prompt="""
-          Summarize the following text into three concise bullet points:
+          Summarize the following text into two concise sentences:
           Artificial intelligence (AI) is the intelligence of machines or software, as opposed to the intelligence of human beings or animals. AI applications include advanced web search engines (e.g., Google Search), recommendation systems (used by YouTube, Amazon, and Netflix), understanding human speech (such as Siri and Alexa), self-driving cars (e.g., Waymo), generative or creative tools (ChatGPT and AI art), and competing at the highest level in strategic games (such as chess and Go).
           Artificial intelligence was founded as an academic discipline in 1956. The field went through multiple cycles of optimism followed by disappointment and loss of funding. After 2012, deep learning surpassed all previous AI techniques, leading to a vast increase in funding and interest.
           The various sub-fields of AI research are centered around particular goals and the use of particular tools. The traditional goals of AI research include reasoning, knowledge representation, planning, learning, natural language processing, perception, and support for robotics. General intelligence (the ability to solve an arbitrary problem) is among the field's long-term goals.
