f9736d66d3eeb3c7b54be4039426d61b0cf8756d
ahmethaksever <107223224+ahmethaksever@users.noreply.github.com>
2023-08-08 01:02:13 +0300

    philosophy teacher answers philosophical questions
    ChatCompletion method is used

---
+++ teacherGPT.py
@@ -0,0 +1,20 @@
+import openai
+
+openai.api_key = "sk-buraya opeanai key eklenecek"
+
+instruction = "What What Socrates was known for?"
+
+# Create a request to the ChatCompletion endpoint
+response = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system",
+     "content": "You are a high school philosophy teacher. Explain philosophical questions, consepts, terms and paradoxes in a simple way to understand. Give a short answer. The answer should 3 sentences at most."},
+    {"role": "user",
+     "content": instruction
+    }
+  ],
+  max_tokens=100
+)
+
+print(response['choices'][0]['message']['content'])
